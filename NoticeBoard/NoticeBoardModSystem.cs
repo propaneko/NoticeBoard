aee7d41860c0a4676ebda64d5e257a0576e18f7d
Ogarek Sebastian <sebastian.ogarek@coig.pl>
2024-09-06 00:51:44 +0200

    first testing iteration

--- NoticeBoard/NoticeBoardModSystem.cs
+++ NoticeBoard/NoticeBoardModSystem.cs
@@ -1,4 +1,5 @@
-﻿using Vintagestory.API.Client;
+﻿using NoticeBoard.src;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
@@ -7,11 +8,15 @@ namespace NoticeBoard
 {
     public class NoticeBoardModSystem : ModSystem
     {
-
+        ICoreClientAPI capi;
         // Called on server and client
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
+            base.Start(api);
+            api.RegisterBlockClass("NoticeBoardBlock", typeof(NoticeBoardBlock));
+            api.RegisterBlockEntityClass("NoticeBoardBlockEntity", typeof(NoticeBoardBlockEntity));
+
             api.Logger.Notification("Hello from template mod: " + api.Side);
         }
 
@@ -24,6 +29,5 @@ namespace NoticeBoard
         {
             api.Logger.Notification("Hello from template mod client side: " + Lang.Get("noticeboard:hello"));
         }
-
     }
 }
---
+++ NoticeBoard/src/GuiDialogCustom.cs
@@ -0,0 +1,36 @@
+﻿using Vintagestory.API.Client;
+
+public class GuiDialogCustom : GuiDialog
+{
+    private string id;
+    public GuiDialogCustom(string dialogTitle, string id, ICoreClientAPI capi) : base(capi)
+    {
+        this.id = id;
+        // Define the size and layout of the GUI window
+        ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.CenterMiddle);
+
+        ElementBounds buttonBounds = ElementBounds.Fixed(10, 40, 80, 20); // Button position and size
+        ElementBounds labelBounds = ElementBounds.Fixed(10, 80, 300, 20); // Label position and size
+
+        // Create the GUI components
+        SingleComposer = capi.Gui
+            .CreateCompo("myCustomGui", dialogBounds)
+            .AddShadedDialogBG(ElementBounds.Fill)
+            .AddDialogTitleBar("Movable GUI", OnTitleBarClose)
+            .AddStaticText(id, CairoFont.WhiteDetailText(), labelBounds)
+            .AddButton("Click me", OnButtonClick, buttonBounds)
+            .Compose();
+    }
+
+    private void OnTitleBarClose()
+    {
+        TryClose(); // Close the GUI when the title bar close button is clicked
+    }
+    private bool OnButtonClick()
+    {
+        capi.ShowChatMessage($"Button clicked! board id: {this.id}");
+        return true;
+    }
+
+    public override string ToggleKeyCombinationCode => null;
+}
